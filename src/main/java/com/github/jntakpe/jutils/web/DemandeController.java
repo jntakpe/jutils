28acc3081be3ad8fe5e0dfcb4c43a8921349c066
jntakpe <jntakpe@gmail.com>
2013-07-21 23:09:37 +0200

    Pour gestion de cmd cafés remplacement Thymeleaf par AngularJS

--- src/main/java/com/github/jntakpe/jutils/web/DemandeController.java
+++ src/main/java/com/github/jntakpe/jutils/web/DemandeController.java
@@ -1,18 +1,13 @@
 package com.github.jntakpe.jutils.web;
 
 import com.github.jntakpe.jutils.domain.Cafe;
-import com.github.jntakpe.jutils.domain.Demande;
 import com.github.jntakpe.jutils.service.CafeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -28,16 +23,8 @@ public class DemandeController {
     private CafeService cafeService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView display() {
-        ModelAndView mv = new ModelAndView("demande");
-        mv.addObject(new Demande());
-        return mv.addObject("cafes", cafeService.categorize());
-    }
-
-    @RequestMapping(value = "/image/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    @ResponseBody
-    public byte[] displayImage(@PathVariable Long id) throws IOException {
-        return cafeService.findOne(id).getImage();
+    public String display() {
+        return "demande";
     }
 
     @RequestMapping(value = "/cafes", method = RequestMethod.GET)
---
+++ src/main/webapp/static/js/angular/cafe/cafe.js
@@ -0,0 +1,26 @@
+var cafeApp = angular.module('cafeApp', []);
+
+cafeApp.factory('CafesFactory', function ($http) {
+    "use strict";
+    return {
+        findAll: function () {
+            return $http.get('demande/cafes');
+        }
+    };
+});
+
+function CafeCtrl($scope, CafesFactory) {
+    "use strict";
+    $scope.cafes = [];
+    CafesFactory.findAll().success(function (data) {
+        var cafe;
+        if (data) {
+            for (cafe in data) {
+                if (data.hasOwnProperty(cafe)) {
+                    data[cafe].nb = 0;
+                }
+            }
+        }
+        $scope.cafes = data;
+    });
+}
