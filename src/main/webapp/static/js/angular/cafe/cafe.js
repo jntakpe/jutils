b9fc9d4d77a285f501037ebfdd1f8866f1b45e29
jguerrin <julien_guerrin@yahoo.fr>
2013-07-23 16:54:30 +0200

    modif css caf

--- src/main/webapp/static/js/angular/cafe/cafe.js
+++ src/main/webapp/static/js/angular/cafe/cafe.js
@@ -9,21 +9,35 @@ cafeApp.factory('CafesFactory', function ($http) {
     };
 });
 
-cafeApp.factory('IntensiteFactory', function () {
+cafeApp.factory('FiltersFactory', function () {
     "use strict";
-    var filters = [];
+    var filters = {};
+    filters.int = [];
     var colors = ['98E9B0', '7ADF98', '34C84D', 'DFCA3F', 'DFAA3F', 'DF843F', 'CA4C1F', 'CD252B', '820005', '000000'];
     for (var i = 0; i < 10; i++) {
-        filters[i] = {
+        filters.int[i] = {
             color: 'color:#' + colors[i],
             force: i + 1,
             dis: true
         };
     }
+
+    filters.mode = {
+        ristretto : true,
+        espresso : true,
+        lungo: true
+    }
+
+    filters.profil = {
+        intense : true,
+        equilibre : true,
+        fruite: true
+    }
+
     return filters;
 });
 
-cafeApp.controller('CafeCtrl', function ($scope, CafesFactory, IntensiteFactory) {
+cafeApp.controller('CafeCtrl', function ($scope, CafesFactory, FiltersFactory) {
     "use strict";
     $scope.cafes = [];
     CafesFactory.findAll().success(function (data) {
@@ -53,7 +67,7 @@ cafeApp.controller('CafeCtrl', function ($scope, CafesFactory, IntensiteFactory)
         return cafe.nb !== 0;
     };
 
-    $scope.filters = IntensiteFactory;
+    $scope.filters = FiltersFactory;
 
     $scope.switchFilter = function (filter) {
         filter.dis = !filter.dis;
@@ -61,6 +75,10 @@ cafeApp.controller('CafeCtrl', function ($scope, CafesFactory, IntensiteFactory)
 
     $scope.resolveColor= function (filter){
         return filter.dis ? 'color:#808681' : filter.color;
+    }
+
+    $scope.resolveBtn= function (btn){
+        return btn ? 'btn' : 'btn btn-inverse';
     }
 });
 
